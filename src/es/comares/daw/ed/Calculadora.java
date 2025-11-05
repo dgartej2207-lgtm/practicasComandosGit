28b4b1be8a6cf6f9c62c5502c8725b084e14c4d9
diurno <dgartej2207@iesmarquesdecomares.com>
2025-11-05 08:56:22 +0100

    Suma

--- src/es/comares/daw/ed/Calculadora.java
+++ src/es/comares/daw/ed/Calculadora.java
@@ -3,8 +3,9 @@ package es.comares.daw.ed;
 public class Calculadora {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("1.Suma");
+		
+		
 	}
 
 }
